a7c1137e9953a546e9f49169ae01bb792e15bf2a
Nicolò Festa <nicolof@monade.io>
2025-03-25 00:57:23 +0100

    fix sizes and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import GameBookEditor from "@/components/game-book-editor"
+import GameBookEditor from "@/components/game-book-editor";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <div className="w-full max-w-7xl">
-        <h1 className="mb-6 text-3xl font-bold">Game Book Editor</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <div className="w-full">
+        <h1 className="mb-6 text-3xl font-bold">YAGE</h1>
         <GameBookEditor />
       </div>
     </main>
-  )
+  );
 }
-
